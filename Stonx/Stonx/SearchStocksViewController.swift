462d32c468cc8848f8bf2455ef7acb3fb96df622
angel zambrano <angeldzambrano99@gmail.com>
2022-10-22 13:53:34 -0700

    added starter code

---
+++ Stonx/Stonx/SearchStocksViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchStocksViewController.swift
+//  Stonx
+//
+//  Created by Angel Zambrano on 10/22/22.
+//
+
+import UIKit
+
+class SearchStocksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
