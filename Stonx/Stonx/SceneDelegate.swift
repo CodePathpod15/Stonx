d5eb82b0a019d3355897d7979d0dbec1bc8e6376
angel zambrano <angeldzambrano99@gmail.com>
2022-10-22 13:47:39 -0700

    added starter code

--- Stonx/Stonx/SceneDelegate.swift
+++ Stonx/Stonx/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let vc = LoginViewController()
+            let vc = StockInfoViewController()
             let root = vc
           
             
---
+++ Stonx/Stonx/StockInfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StockInfoViewController.swift
+//  Stonx
+//
+//  Created by Angel Zambrano on 10/22/22.
+//
+
+import UIKit
+
+
+//TODO: write implementation here
+
+class StockInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+    }
+    
+}
