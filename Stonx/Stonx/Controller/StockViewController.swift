1552e1ee1f9a48b9fdc8987fad058ff561373c9e
angel zambrano <angeldzambrano99@gmail.com>
2022-10-30 13:50:22 -0700

    added the stock viewcontroller

--- Stonx/Stonx/Controller/StockViewController.swift
+++ Stonx/Stonx/Controller/StockViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
-
+import Parse
 /// this contains a single view controller
 class StockViewController: UIViewController {
     
+    var stock: PFObject? = nil
+    
+    // Initializers
+       init(stock: PFObject) {
+           super.init(nibName: nil, bundle: nil)
+           self.stock = stock
+       }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor
+        view.backgroundColor = .white
 
     }
 
