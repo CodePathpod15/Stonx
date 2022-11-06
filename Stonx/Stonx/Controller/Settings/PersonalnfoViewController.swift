acde5936c50cb211ace2f0fb3324af92b1ec6de4
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2022-11-06 14:18:38 -0800

    Allow users to change usernameand display correct user info

--- Stonx/Stonx/Controller/Settings/PersonalnfoViewController.swift
+++ Stonx/Stonx/Controller/Settings/PersonalnfoViewController.swift
@@ -4,48 +4,33 @@
 //
 //  Created by Angel Zambrano on 10/31/22.
 //
-
+import Parse
 import UIKit
-// TODO: present errors. For example when information is incorrect. 
+
 class PersonalnfoViewController: UIViewController {
     let tableview = UITableView(frame: .zero, style: .grouped)
     let settings = ["username", "full name"]
-    
+    let currentUser = PFUser.current()!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
         
         viewSetups()
         addconstraints()
     }
-    
-    private func viewSetups(){
+        
+    private func viewSetups() {
         tableview.translatesAutoresizingMaskIntoConstraints = false
         tableview.register(BalanceTableViewCell.self, forCellReuseIdentifier: BalanceTableViewCell.identifier)
         tableview.dataSource = self
         tableview.delegate = self
         view.addSubview(tableview)
-        
     }
     
     private func addconstraints() {
         tableview.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
-        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension PersonalnfoViewController: UITableViewDataSource {
@@ -53,59 +38,54 @@ extension PersonalnfoViewController: UITableViewDataSource {
         return 2
     }
     
+    // Username and email
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: BalanceTableViewCell.identifier, for: indexPath) as! BalanceTableViewCell
-        // configuring the title
         if indexPath.row == 0 {
             cell.accessoryType = .disclosureIndicator
-            cell.configure(with: "username")
-            cell.configure(name: "angelzzz23")
+            cell.configure(with: "Username")
+            cell.configure(name: currentUser.username!)
             return cell
         } else {
             cell.accessoryType = .disclosureIndicator
-            cell.configure(with: "Full Name")
-            cell.configure(name: "Angel Zambrano")
+            cell.configure(with: "Email")
+            cell.configure(name: currentUser.email!)
+            cell.isUserInteractionEnabled = false
         }
         
-        
         return cell
-        
     }
-    
-    
 }
 
 extension PersonalnfoViewController: UITableViewDelegate {
     // you might want to get the name of the user from parse and show it in the textfield
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let setting = settings[indexPath.row]
-        
-        let alertController: UIAlertController = UIAlertController(title: "Editing \(setting)", message: "", preferredStyle: .alert)
+        if indexPath.row == 0 {
+            let setting = "Username"
+            
+            let alertController = UIAlertController(title: "Editing \(setting)", message: "", preferredStyle: .alert)
 
-            //cancel button
-            let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { action -> Void in
-                //cancel code
-                
+            // cancel button
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+                // cancel code
             }
             alertController.addAction(cancelAction)
 
-            //Create an optional action
-        let nextAction: UIAlertAction = UIAlertAction(title: "Ok", style: .default) { action -> Void in
+            // Save the new username
+            let nextAction = UIAlertAction(title: "Ok", style: .default) { _ in
                 let text = (alertController.textFields?.first as! UITextField).text
-            
-                // TODO: update this
-                print(text)
                 
+                self.currentUser["Username"] = text
+                self.currentUser.saveInBackground()
             }
-        
+            
             alertController.addAction(nextAction)
-        
-        alertController.addTextField { (textField) in
-            textField.placeholder = "Enter \(setting)"
+            
+            alertController.addTextField { textField in
+                textField.placeholder = "Enter \(setting)"
+            }
+            
+            present(alertController, animated: true)
         }
-        
-        present(alertController, animated: true)
-    
-        
     }
 }
