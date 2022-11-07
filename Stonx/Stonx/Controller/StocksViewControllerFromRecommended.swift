bfb01ed64a9868d8937e512a513fbd3c807beaac
angel zambrano <angeldzambrano99@gmail.com>
2022-11-06 20:08:20 -0800

    added the viewing of a stock implementation
    created a convience initializer

---
+++ Stonx/Stonx/Controller/StocksViewControllerFromRecommended.swift
@@ -0,0 +1,26 @@
+//
+//  StocksViewControllerFromRecommended.swift
+//  Stonx
+//
+//  Created by Angel Zambrano on 11/6/22.
+//
+
+import UIKit
+
+
+// here is the implementation of the
+
+
+extension StocksViewController {
+    convenience init(symbol: String, full_name: String) {
+        
+        let bestMatch = BestMatch(the1Symbol: symbol, the2Name: full_name, the3Type: "", the4Region: "", the5MarketOpen: "", the6MarketClose: "", the7Timezone: "", the8Currency: "", the9MatchScore: "")
+    
+        self.init(stockInfo: bestMatch)
+    
+        
+    }
+    
+    
+    
+}
